0091ef6208e09143d21676a1b4cb5f5cb728a95a
Isaac <git@eartharoid.me>
2021-05-12 14:54:49 +0100

    Attempt to do good types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,7 @@ declare module 'leekslazylogger' {
 
 	interface LogLevel {
 		type?: 'log' | 'info' | 'debug' | 'warn' | 'error',
-		format: string
+		format: string | function
 	}
 
 	interface LoggerOptions {
@@ -16,14 +16,52 @@ declare module 'leekslazylogger' {
 		debug?: boolean,
 		daily?: boolean,
 		directory?: string,
-		levels: object<LogLevel>
+		levels?: {
+			[level: string]: LogLevel
+		}
 	}
 
-	/**
-	 * @description Logger object with options
-	 * @param {LoggerOptions} options - customise your logger with options
-	 */
+	let options: LoggerOptions;
 	export default class Logger {
-		constructor(options?: LoggerOptions);
+		public options: LoggerOptions;
+		public ext: Array;
+
+		/**
+		 * @description Create a new Logger
+		 * @param {LoggerOptions} o - customise your logger with options
+		 */
+		constructor(o?: LoggerOptions) {
+			options = o;
+		};
+		
+		/**
+		 * @description 
+		 * @param {any} text - the content to log, a string or anything that can be stringified
+		 * @param {any} [colour] - colours
+		 * @param {any} [args] - stuff to add on the end, won't be formatted
+		 */
+		public [level in options.levels]: (text: any, colour: any, ...args: any) => void;
+
+		public timestamp(): string;
+
+		public register(name: string, o: any): void;
+
+		private verbose(text: string): void;
+
+		private clearOldFiles(callback: function): void;
+
+		private prepareFile(): void;
+
+		private writeLine(type: string, str: string, ...args: any): void;
+
+		get baseFileName(): string;
+
+		get stdoutPath(): string;
+
+		get stderrPath(): string;
+
+		public static format(str: string): string;
+
+		public static f(str: string): string;
 	}
 }
